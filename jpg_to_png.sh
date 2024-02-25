6e7d771ae388adc2a12a96b39119f246823549a7
Jonathas Gabriel Davide Tenorio <2009641@aluno.univesp.br>
2024-02-25 09:18:28 -0300

    Nome alterado

---
+++ jpg_to_png.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+converte_imagem(){
+cd imagens-livros/
+
+if [ ! -d png ]
+then 
+    mkdir png
+fi
+
+for image in *.jpg
+do
+    local imagem_sem_extensao=$(ls $imagem | awk -F. '{ print $1 }')
+    convert $imagem_sem_extensao.jpg png/$imagem_sem_extensao.png
+done
+}
+
+converte_imagem 2>erros_conversao.txt
+if [ $? -eq 0 ]
+then
+    echo "Conversão realizada com sucesso"
+else
+    echo "Houve uma falha no processo de conversão"
+fi
