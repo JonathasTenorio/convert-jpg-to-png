3919071f338cc8764cc190452f45d56115aef987
Jonathas Gabriel Davide Tenorio <2009641@aluno.univesp.br>
2024-02-26 09:27:57 -0300

    Adição de comentários ao código

--- jpg_to_png.sh
+++ jpg_to_png.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
 converte_imagem(){
-cd imagens-livros/
+    cd imagens-livros/ # Entra no diretório imagens-livros
 
-if [ ! -d png ]
-then 
-    mkdir png
-fi
+    if [ ! -d png ] # verifíca se o diretório png não existe
+    then 
+        mkdir png # cria o diretório png
+    fi
 
-for image in *.jpg
-do
-    local imagem_sem_extensao=$(ls $image | awk -F. '{ print $1 }')
-    convert $imagem_sem_extensao.jpg png/$imagem_sem_extensao.png
-done
-}
+    for image in *.jpg # itera sobre os arquivos com extensão jpg
+    do
+        local imagem_sem_extensao=$(ls $image | awk -F. '{ print $1 }') # recebe o nome do arquivo e armazena o nome sem a extensão
+        convert $imagem_sem_extensao.jpg png/$imagem_sem_extensao.png #converte o arquivo para png e armazena dentro do diretorio png
+    done
+    }
 
-converte_imagem 2>erros_conversao.txt
-if [ $? -eq 0 ]
+converte_imagem 2>erros_conversao.txt # Chama a função e armazena os erros em um arquivo chamado erros_conversao.txt
+
+if [ $? -eq 0 ] #verifica o resultado da execução da função
 then
-    echo "Conversão realizada com sucesso"
+    echo "Conversão realizada com sucesso" # Não houve erros, resultado igual a 0
 else
-    echo "Houve uma falha no processo de conversão"
+    echo "Houve uma falha no processo de conversão" # Houve erros, resultado foi igual a 1
 fi
